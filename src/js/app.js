d3b28f810ea34029436a17f1217a3d45f6b1f186
Dmitriy Lozichnyy <dlozichnyy2005@gmail.com>
2024-02-21 10:30:19 +0700

    done

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import { orderByProps } from "./orderByProps";
+
+export { orderByProps };
---
+++ src/js/orderByProps.js
@@ -0,0 +1,22 @@
+export function orderByProps(obj, order) {
+  const orderProps = [];
+  const restProps = [];
+
+  order.forEach((prop) => {
+    if (prop in obj) {
+      orderProps.push({ key: prop, value: obj[prop] });
+    }
+  });
+
+  for (let prop in obj) {
+    if (!order.includes(prop)) {
+      restProps.push({ key: prop, value: obj[prop] });
+    }
+  }
+
+  restProps.sort((a, b) => {
+    return a.key.localeCompare(b.key);
+  });
+
+  return orderProps.concat(restProps);
+}
